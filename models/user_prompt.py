dfec74bec4575cbe9447cfbe6fffd518383e9216
chiefkarim <krimotop2016@gmail.com>
2025-02-27 16:57:03 +0100

    feat: add chat history

--- models/user_prompt.py
+++ models/user_prompt.py
@@ -4,4 +4,4 @@ from typing import List
 
 class UserPrompt(BaseModel):
     prompt: str
-    chatHistory: List[str]
+    chatHistory: List[object]
--- utils/chat.py
+++ utils/chat.py
@@ -9,21 +9,26 @@ API_KEY = os.getenv('API_KEY')
 if not API_KEY:
     raise ValueError('Please add API_KEY to .env file!')
 
+client = OpenAI(
+        api_key=API_KEY,
+        base_url="https://generativelanguage.googleapis.com/v1beta/openai/"
+    )
 # TODO: make the streaming faster
 # first stream is taking some time which makes the user experince bad
 
 
 # TODO: type the function returned data
 
+
+# TODO: figure out a way to presist messages
+
+
 def chat(prompt: UserPrompt):
-    client = OpenAI(
-        api_key=API_KEY,
-        base_url="https://generativelanguage.googleapis.com/v1beta/openai/"
-    )
     response = client.chat.completions.create(
         model="gemini-2.0-flash",
         n=1,
         messages=[
+            prompt.chatHistory,
             {"role": "system", "content": "You are a helpful assistant."},
             {
                 "role": "user",
@@ -31,6 +36,7 @@ def chat(prompt: UserPrompt):
         ],
         stream=True
     )
+
     for chunk in response:
         yield f"{chunk.choices[0].delta.content or ''}"
 
